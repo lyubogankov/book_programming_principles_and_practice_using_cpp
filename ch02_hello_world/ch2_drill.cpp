a810d54888bdfd31e84fb8337d1cf0e16ff38b3a
gankovl <gankovl@oregonstate.edu>
2022-10-18 20:43:52 -0700

    ch2_drill.cpp: added notes about how to compile on my laptop using g++
    .gitignore: ignoring .exe and .out files

--- ch02_hello_world/ch2_drill.cpp
+++ ch02_hello_world/ch2_drill.cpp
@@ -1,5 +1,9 @@
 #include "std_lib_facilities.h"
 
+// compilation on my laptop:
+//  g++ -o [name].exe ch2_drill.cpp   * if a particular name is desired
+//  g++ ch2_drill.cpp                 * if default name a.out is ok
+
 int main() {
     cout << "Hello, World!\n";
     keep_window_open();
