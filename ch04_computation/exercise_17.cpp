21e9264d162f9d7a19e4d0def259aaa61c5aaec3
gankovl <gankovl@oregonstate.edu>
2022-11-10 20:53:34 -0800

    pseudocode for ex17

---
+++ ch04_computation/exercise_17.cpp
@@ -0,0 +1,11 @@
+/*
+Write a program that finds the min, max, and mode of a sequence of strings.
+*/
+
+
+/*
+Idea -- start with an empty vector.
+For each string seen, add a vector of length 2 to the parent vector -- 1st idx = string, 2nd idx = count.
+
+Once user is done entering strings, traverse parent vector and find min/max/mode!
+*/
