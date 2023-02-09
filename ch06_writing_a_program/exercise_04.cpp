e4a41ebbca822bb173ceb390bb031d0261e84c0f
lyubogankov <lyubogankov13@gmail.com>
2023-02-09 08:04:50 -0800

    completed ex2

---
+++ ch06_writing_a_program/exercise_04.cpp
@@ -0,0 +1,4 @@
+/*
+4. Define a class Name_value that holds a string and a value.
+   Rework exercise 19 in Chapter 4 to use a vector<Name_value> instead of two vectors.
+*/
---
+++ ch06_writing_a_program/exercise_09.cpp
@@ -0,0 +1,8 @@
+/*
+ 9. Write a program that reads digits and composes them into integers. 
+ 
+    For example, 123 is read as the characters 1, 2, and 3. The program should output 123 is 1 hundred and 2 tens and 3 ones.
+    The number should be output as an int value. Handle numbers with one, two, three, or four digits.
+    
+    Hint: To get the integer value 5 from the character '5' subtract '0', that is, '5'–'0'==5.
+*/
