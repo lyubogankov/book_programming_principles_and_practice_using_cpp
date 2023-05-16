98f0650224d7583e973da408acc18f442480946d
lyubogankov <lyubogankov13@gmail.com>
2023-05-15 20:49:04 -0700

    completed ex14

--- ch09_technicalities_classes_etc/ex_14-17.cpp
+++ ch09_technicalities_classes_etc/ex_14-17.cpp
@@ -15,7 +15,65 @@
 16. Define an input operator (>>) that reads monetary amounts with currency denominations, such as USD1.23 and DKK5.00, into a Money variable.
     Also define a corresponding output operator (>>).
 
+Discussion qs
+-------------
+
 17. Give an example of a calculation where a Rational gives a mathematically better result than Money.
 
 18. Give an example of a calculation where a Rational gives a mathematically better result than double.
 */
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Money {
+    public:
+        Money(string denomination, double amount);
+        // // generic term = ? whole units vs fractions/remainder as hundredth (multiplied by 100)
+        // Money(string denomination, int dollars, int cents);
+        Money(string denomination, int wholes, int hundredths);
+        string denomination() { return _denomination; }
+        int wholes() { return _hundredths / 100; }
+        int hundredths() { return _hundredths % 100; }
+    private:
+        long int _hundredths {0};
+        string _denomination;
+};
+Money::Money(string denomination, int wholes, int hundredths) :
+    _denomination {denomination},
+    _hundredths { hundredths + wholes*100 }
+{}
+Money::Money(string denomination, double amount) : _denomination {denomination} 
+{
+    // odd behavior: previously had floor(amount*100), but that consistently chopped off a whole cent.
+    //               I also tested int(amount*100), and it had the same effect.
+    _hundredths += int(amount*1000)/ 10; // whole + hundredths
+    // "4/5 rounding" rule for fractional cents
+    if ((int(amount*1000) % 10) >= 5)
+        _hundredths += 1;
+}
+
+ostream& operator<<(ostream& os, Money m) {
+    // https://stackoverflow.com/a/1714538
+    return os << m.denomination() << m.wholes() << "." 
+              << setfill('0') << setw(2)    // want leading zero!
+              << m.hundredths()
+              << setfill(' ');                // reset to default, bc apparently it's persistent
+}
+
+int main() {
+
+    Money d1 {"$", 10.04};
+    Money d2 {"$", 10, 4};
+    Money d3 {"DKK", 5.123};
+    Money d4 {"DKK", 5.987};
+
+    cout << "d1: " << d1 << "\n"
+         << "d2: " << d2 << "\n"
+         << "d3: " << d3 << "\n"
+         << "d4: " << d4 << "\n";
+
+    return 0;
+}
