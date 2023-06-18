34d4e2afc92101b0d21c493a1ad87a0312e91d8f
lyubogankov <lyubogankov13@gmail.com>
2023-06-17 20:36:21 -0700

    completed ex01 - file contents -> lowercase

--- ch11_customizing_io/ex_01.cpp
+++ ch11_customizing_io/ex_01.cpp
@@ -1,3 +1,30 @@
 /*
 1. Write a program that reads a text file and converts its input to all lower case, producing a new file.
 */
+
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void filetolower(string srcfile = "ex_01_input.txt", string destfile = "ex_01_output.txt") {
+    ifstream ifs {srcfile};
+    if (!ifs) {
+        cout << "Could not open file for reading (" << srcfile << ")\n";
+        return;
+    }
+    ofstream ofs {destfile};
+    if (!ofs) {
+        cout << "Could not open file for writing (" << destfile << ")\n";
+        return;
+    }
+    char c;
+    while(ifs >> noskipws >> c)
+        ofs << (char)tolower(c);
+}
+
+int main() {
+    filetolower();
+    return 0;
+}
