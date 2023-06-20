a855407b381c9d4cdb563a33e6a20f56714c45bf
lyubogankov <lyubogankov13@gmail.com>
2023-06-19 21:12:59 -0700

    completed ex13!

--- ch11_customizing_io/ex_12-13.cpp
+++ ch11_customizing_io/ex_12-13.cpp
@@ -7,3 +7,36 @@
     For example, Norwegian Blue parrot becomes parrot Blue Norwegian. 
     You are allowed to assume that all the strings from the file will fit into memory at once.
 */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void ex13(const string& src="ex_12-13_input.txt", const string& dst="ex_13_output.txt") {
+    // This function will reverse the order, but it will not preserve the type of whitespace
+    // encountered between words.  In the output, all words will be separated by space (' ').
+    ifstream ifs {src};
+    if (!ifs) throw runtime_error("Could not open file for reading (" + src + ")");
+    ofstream ofs {dst};
+    if (!ofs) throw runtime_error("Could not open file for writing (" + dst + ")");
+    
+    vector<string> contents;
+    string s;
+    while(ifs >> s)
+        contents.push_back(s);
+
+    for(int i=contents.size()-1; i >= 0; i--) {
+        ofs << contents[i];
+        if (i > 0)
+            ofs << ' ';
+    }
+}
+
+int main() {
+
+    ex13();
+
+    return 0;
+}
