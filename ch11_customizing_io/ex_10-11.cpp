27c47ad71dbe5706f9a5f0144eb9393b67e46532
lyubogankov <lyubogankov13@gmail.com>
2023-06-19 20:41:48 -0700

    initial attempt at ex11

--- ch11_customizing_io/ex_10-11.cpp
+++ ch11_customizing_io/ex_10-11.cpp
@@ -22,19 +22,57 @@ vector<string> split(const string& s) {
     return split;
 }
 
-// vector<string> split(const string& s, const string& w) {
 
-// }
+bool string_contains_char(const string& s, const char& c) {
+    for( char _c : s)
+        if (_c == c)
+            return true;
+    return false;
+}
+
+vector<string> split(const string& s, const string& w) {
+    istringstream iss {s};
+    vector<string> split = {""};
+    int current_idx = 0;
+    int i;
+    string substr;
+    // break up the input string by normal whitespace 
+    while(iss >> substr) {
+        // For each whitespace-separated substring, iterate over each character.
+        i = 0;
+        for (char c : substr) {
+            if (string_contains_char(w, c)) {
+                current_idx += 1;
+                // if (i < substr.size())
+                split.push_back("");
+            } else
+                split[current_idx].push_back(c);
+            ++i;
+        }
+    }
+
+    return split;
+}
 
 int main() {
 
     string original_string = "this is a whitespace separated-string\nwoah\twhoosh";
+    
+    // using first function
     cout << "original: " << original_string << "\n";
     cout << "split:   |";
     vector<string> split_string = split(original_string);
     for (string& substr : split_string)
         cout << substr << "|";
-    cout << "\n";
+    cout << "\n\n";
+
+    // using second function
+    cout << "original: " << original_string << "\n";
+    cout << "split:   |";
+    vector<string> split_string_two = split(original_string, "aeiou");
+    for (string& substr : split_string_two)
+        cout << substr << "|";
+    cout << "\n\n";
 
     return 0;
 }
