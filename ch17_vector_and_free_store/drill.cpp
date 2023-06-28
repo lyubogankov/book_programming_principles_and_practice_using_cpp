0dc0d555580880e31a907e01fc6663fe0a108d15
lyubogankov <lyubogankov13@gmail.com>
2023-06-27 22:18:10 -0700

    completed through part2 #9

--- ch17_vector_and_free_store/drill.cpp
+++ ch17_vector_and_free_store/drill.cpp
@@ -17,17 +17,11 @@ The second part focuses on pointers and their relation to arrays. Using print_ar
 1. Allocate an int, initialize it to 7, and assign its address to a variable p1.
 2. Print out the value of p1 and of the int it points to.
 3. Allocate an array of seven ints; initialize it to 1, 2, 4, 8, etc.; and assign its address to a variable p2.
-
 4. Print out the value of p2 and of the array it points to.
-
 5. Declare an int* called p3 and initialize it with p2.
-
 6. Assign p1 to p2.
-
 7. Assign p3 to p2.
-
 8. Print out the values of p1 and p2 and of what they point to.
-
 9. Deallocate all the memory you allocated from the free store.
 
 10. Allocate an array of ten ints; initialize it to 1, 2, 4, 8, etc.; and assign its address to a variable p1.
@@ -107,7 +101,7 @@ void allocate_and_print_vector(int size) {
 // PART TWO
 //
 
-void part_two_array() {
+void part_two() {
     cout << "\n\n---- PART TWO\n";
     
     // 1.
@@ -119,16 +113,33 @@ void part_two_array() {
     int p2_size = 7;
     int* p2 = new int[p2_size];
     for (int i=0; i<p2_size; i++)
-        p2[i] = pow(2, i+1);
+        p2[i] = pow(2, i);
+    // 4.
     cout << "p2: ";
     print_array(cout, p2, p2_size);
     cout << "p2 address: " << p2 << "\n";
 
-    // cleanup
+    // 5.
+    int* p3 = p2;
+    // 6.
+    p2 = p1;
+    p2 = p3;
+    // 7.
+    cout << "p1 address: " << p1 << "\n";
+    cout << "p2 address: " << p2 << "\n";
+    cout << "p1 contents: " << *p1 << "\n";
+    cout << "p2 contents: ";
+    print_array(cout, p2, p2_size);
+
+    // cleanup (9.)
     delete[] p2;
 }
-// void part_two_vector() {
-// }
+
+void part_two_array() {
+
+}
+void part_two_vector() {
+}
 
 
 int main() {
@@ -144,6 +155,9 @@ int main() {
     allocate_and_print_vector(20);
     
     // Part Two
+    part_two();
+    part_two_array();
+    part_two_vector();
 
     return 0;
 }
