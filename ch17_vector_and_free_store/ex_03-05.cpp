a40e2a1ec0e47a0b106bf76736b59b62ca1c9670
lyubogankov <lyubogankov13@gmail.com>
2023-06-29 09:10:44 -0700

    populating problem descriptions

---
+++ ch17_vector_and_free_store/ex_03-05.cpp
@@ -0,0 +1,14 @@
+/*
+3. Write a function, void to_lower(char* s), 
+    that replaces all uppercase characters in the C-style string s with their lowercase equivalents. 
+    For example, `Hello, World!` becomes `hello, world!`.
+    Do not use any standard library functions. 
+    A C-style string is a zero-terminated array of characters, so if you find a char with the value 0 you are at the end.
+
+4. Write a function, char* strdup(const char*), 
+    that copies a C-style string into memory it allocates on the free store. 
+    Do not use any standard library functions.
+
+5. Write a function, char* findx(const char* s, const char* x), 
+    that finds the first occurrence of the C-style string x in s.
+*/
---
+++ ch17_vector_and_free_store/ex_07-08_10.cpp
@@ -0,0 +1,13 @@
+/*
+7. Write a program that reads characters from cin into an array that you allocate on the free store. 
+    Read individual characters until an exclamation mark (!) is entered. 
+    Do not use a std::string. Do not worry about memory exhaustion.
+
+8. Do exercise 7 again, but this time read into a std::string 
+    rather than to memory you put on the free store (string knows how to use the free store for you).
+
+10. Look at your solution of exercise 7. 
+    Is there any way that input could get the array to overflow; 
+    that is, is there any way you could enter more characters than you allocated space for (a serious error)? 
+    Does anything reasonable happen if you try to enter more characters than you allocated?
+*/
