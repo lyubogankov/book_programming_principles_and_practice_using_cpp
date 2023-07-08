b2346e690781addd92fc276a98b1d0efa6e468dc
lyubogankov <lyubogankov13@gmail.com>
2023-07-08 15:14:53 -0700

    completed ex07, 08

--- ch17_vector_and_free_store/ex_07-08_10.cpp
+++ ch17_vector_and_free_store/ex_07-08_10.cpp
@@ -11,3 +11,45 @@
     that is, is there any way you could enter more characters than you allocated space for (a serious error)? 
     Does anything reasonable happen if you try to enter more characters than you allocated?
 */
+
+#include <limits>
+#include <iostream>
+#include <string>
+using namespace std;
+
+constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
+
+void ex_07() {
+    char* userinput = new char[1000];
+
+    char c;
+    int idx = 0;
+
+    cout << "\nPlease enter characters, terminate with '!':\n";
+    while(cin >> noskipws >> c && c != '!') {
+        // cout << "    " << int(c) << '\n';
+        userinput[idx++] = c;
+    }
+    cin.ignore(max_size, '\n'); // "eat" all remaining input after first exclamation point.
+    cout << "Here is what you entered:\n" << userinput << '\n';
+
+    delete[] userinput;
+}
+
+void ex_08() {
+    string userinput;
+    char c;
+    cout << "\nPlease enter characters, terminate with '!':\n";
+    while(cin >> noskipws >> c && c != '!') {
+        // cout << "    " << int(c) << '\n';
+        userinput.push_back(c);
+    }
+    cin.ignore(max_size, '\n');
+    cout << "Here is what you entered:\n" << userinput << '\n';
+}
+
+int main() {
+    ex_07();
+    ex_08();
+    return 0;
+}
