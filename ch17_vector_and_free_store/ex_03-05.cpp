a91bcb0b5384f2ec4ec9d3d2a0069b3f025687eb
lyubogankov <lyubogankov13@gmail.com>
2023-07-08 19:25:46 -0700

    completed ex 03 - 05!

--- ch17_vector_and_free_store/ex_03-05.cpp
+++ ch17_vector_and_free_store/ex_03-05.cpp
@@ -12,3 +12,79 @@
 5. Write a function, char* findx(const char* s, const char* x), 
     that finds the first occurrence of the C-style string x in s.
 */
+
+#include <iostream>
+using namespace std;
+
+// ex 03
+void to_lower(char* s) {
+    for (int i=0; s[i] != 0; i++)
+        if (65 <= s[i] && s[i] <= 90)
+            s[i] += 32;  // ascii: A = 65, a = 97
+}
+
+// ex 04
+char* strdup(const char* s) {
+    int size = 0;
+    for (size=0; s[size] != 0; size++); // determine char array size
+    
+    char* ret = new char[size];
+    for (int i=0; i<=size; i++)
+        ret[i] = s[i];
+
+    return ret;
+}
+
+// ex 05
+char* findx(/*const*/ char* s, const char*x, bool printout=true) {
+    
+    // // Before I read the problem carefully:
+    // this will return the (zero-indexed) index within `s` 
+    //      of the first character in `x` if a match is found, otherwise `-1`.
+    // ex: s = "Hello world", x = "world"
+    //                ^ ret = 6
+
+    // After seeing the return value, I guess I need to return a pointer 
+    // to the start of the substring if it is found, otherwise `nullptr`?
+
+    // Ok -- I tried this, and got a clangtidy error about converting from const char* -> char *...
+
+    char* ret = nullptr;
+    int j=0;
+    for(int i=0; s[i] != 0; i++) {
+        if (printout)
+            cout << "    s[i]: " << s[i] << "    x[j]: " << x[j] << '\n';
+        if (s[i] == x[j])
+            j++;
+        else if (x[j] == 0)
+            ret = &(s[i-j]);
+        else
+            j = 0;
+    }
+    return ret;
+}
+
+int main() {
+
+    // ex 03
+    // not sure whether I need to add the null terminator myself.
+    // it seems that the null terminator is automatically added
+    char s[] = "Hello, World!";
+    cout << "before: " << s << "\n";
+    to_lower(s);
+    cout << "after:  " << s << "\n";
+
+    // ex 04
+    char* copied = strdup(s);
+    cout << "original: " << s << '\n'
+         << "copy:     " << copied << '\n';
+    delete[] copied;
+
+    // ex 05
+    char* substr = findx(s, "world");
+    cout << "'world' within '" << s << "'? " << (substr != nullptr) << '\n';
+    substr = findx(s, "meme");
+    cout << "'meme' within '" << s << "'? " << (substr != nullptr) << '\n';
+
+    return 0;
+}
