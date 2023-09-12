b6925608b9331187dbabf15a8380c4da7ed4d3fd
lyubogankov <lyubogankov13@gmail.com>
2023-09-12 07:41:42 -0700

    completed first part of ex13

--- ch19_vectors_templates_and_exceptions/ex_13.cpp
+++ ch19_vectors_templates_and_exceptions/ex_13.cpp
@@ -7,3 +7,70 @@
     
     Then add a copy constructor and a copy assignment so that you can use Tracer objects to see when copying is done.
 */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+
+class Tracer {
+private:
+    string constructor_string, destructor_string;
+public:
+    Tracer(string constructor_str, string destructor_str)
+        : constructor_string {constructor_str}, destructor_string {destructor_str} {
+            cout << constructor_string << '\n';
+        }
+    ~Tracer() {
+        cout << destructor_string << '\n';
+    }
+};
+
+struct MyStruct {
+private:
+    Tracer t;
+public:
+    MyStruct(string constructor_str, string destructor_str)
+        : t {Tracer(constructor_str, destructor_str)} {
+            cout << "- Just made a MyStruct.\n";
+        }
+    ~MyStruct() {
+        cout << "- Just destroyed a MyStruct.\n";
+    }
+};
+
+Tracer global_tracer {"--- global tracer constructor", "--- global tracer destructor"};
+
+// local objects on stack, on heap
+void fn_stack() {
+    cout << "\nRunning fn_stack()\n";
+    Tracer stack_tracer {"--- stack tracer constructor", "--- stack tracer destructor"};
+    cout << "Exiting fn_stack()\n\n";
+}
+Tracer* fn_heap_creation() {
+    cout << "\nRunning fn_heap_creation()\n";
+    Tracer* heap_tracer_ptr = new Tracer("--- heap tracer constructor", "--- heap tracer destructor");
+    cout << "Exiting fn_heap_creation()\n\n";
+    return heap_tracer_ptr;
+}
+void fn_heap_deletion(Tracer* heap_tracer_ptr) {
+    cout << "\nRunning fn_heap_deletion()\n";
+    delete heap_tracer_ptr;
+    cout << "Exiting fn_heap_deletion()\n\n";
+}
+void stack_mystruct() {
+    cout << "\nRunning stack_mystruct()\n";
+    MyStruct ms {"--- MyStruct member constructor", "--- MyStruct member destructor"};
+    cout << "Exiting stack_mystruct()\n\n";
+}
+
+int main() {
+    cout << "\nRunning main()\n";
+    fn_stack();
+    Tracer* heap_tracer_ptr = fn_heap_creation();
+    fn_heap_deletion(heap_tracer_ptr);
+    stack_mystruct();
+    cout << "Exiting main()\n\n";
+    return 0;
+}
